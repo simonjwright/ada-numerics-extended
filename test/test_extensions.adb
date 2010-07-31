dea8f6e049900fe0a0214e78e264518aa7f68060
Simon Wright <simonjwright@users.sourceforge.net>
2010-07-31 17:04:42 +0100

    Included test16 from people.sc.fsu.edu/~jburkardt.

--- test/test_extensions.adb
+++ test/test_extensions.adb
@@ -19,7 +19,7 @@ with Ada.Numerics.Generic_Complex_Arrays.Extensions;
 
 procedure Test_Extensions is
 
-   subtype My_Float is Float range -100.0 .. 100.0;
+   subtype My_Float is Float;
 
    package Real_Arrays
    is new Ada.Numerics.Generic_Real_Arrays (My_Float);
@@ -30,24 +30,66 @@ procedure Test_Extensions is
    package Extensions
    is new Complex_Arrays.Extensions;
 
+   use Real_Arrays;
+   use Complex_Types;
    use Complex_Arrays;
-
-   Input : Complex_Matrix (1 .. 3, 1 .. 3);
-   Result : Complex_Vector (1 .. Input'Length (1));
 
 begin
 
+   declare
    --  Values in yc's example
-   Input := (((8.0, 0.0), (-1.0, 0.0), (-5.0, 0.0)),
-             ((-4.0, 0.0), (4.0, 0.0), (-2.0, 0.0)),
-             ((18.0, 0.0), (-5.0, 0.0), (-7.0, 0.0)));
+      Input : constant Complex_Matrix
+        := (((8.0, 0.0), (-1.0, 0.0), (-5.0, 0.0)),
+            ((-4.0, 0.0), (4.0, 0.0), (-2.0, 0.0)),
+            ((18.0, 0.0), (-5.0, 0.0), (-7.0, 0.0)));
+      Result : Complex_Vector (1 .. Input'Length (1));
+   begin
+      Result := Extensions.Eigenvalues (Input);
 
-   Result := Extensions.Eigenvalues (Input);
+      for J in Result'Range loop
+         Put_Line (My_Float'Base'Image (Result (J).Re)
+                     & " "
+                     & My_Float'Base'Image (Result (J).Im));
+      end loop;
+      New_Line;
+   end;
 
-   for J in Result'Range loop
-      Put_Line (My_Float'Base'Image (Result (J).Re)
-                  & " "
-                  & My_Float'Base'Image (Result (J).Im));
-   end loop;
+   declare
+   --  Values in Test 16 of
+   --  http://people.sc.fsu.edu/~jburkardt/f_src/lapack/lapack_OSX_prb_output.txt
+      Z : constant Complex := (0.0, 0.0);
+      A : constant Complex := (2.44949, 0.0);
+      B : constant Complex := (3.16228, 0.0);
+      C : constant Complex := (3.46410, 0.0);
+      Input : constant Complex_Matrix
+        := (
+            1 => (Z, A, Z, Z, Z, Z, Z),
+            2 => (A, Z, B, Z, Z, Z, Z),
+            3 => (Z, B, Z, C, Z, Z, Z),
+            4 => (Z, Z, C, Z, C, Z, Z),
+            5 => (Z, Z, Z, C, Z, B, Z),
+            6 => (Z, Z, Z, Z, B, Z, A),
+            7 => (Z, Z, Z, Z, Z, A, Z)
+           );
+   begin
+      declare
+         Result : constant Real_Vector := Complex_Arrays.Eigenvalues (Input);
+      begin
+         for J in Result'Range loop
+            Put_Line (My_Float'Base'Image (Result (J)));
+         end loop;
+      end;
+      New_Line;
+      declare
+         Result : constant Complex_Vector := Extensions.Eigenvalues (Input);
+      begin
+         for J in Result'Range loop
+            Put_Line (My_Float'Base'Image (Result (J).Re)
+                        & " "
+                        & My_Float'Base'Image (Result (J).Im));
+         end loop;
+      end;
+      New_Line;
+   end;
 
 end Test_Extensions;
